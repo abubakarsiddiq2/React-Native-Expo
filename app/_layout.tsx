51705f10665cb6fe4000cd8bad25f0a9b1d1fd35
mabubakarsiddiq11 <mabubakarsiddiq11@gmail.com>
2025-09-01 12:16:00 +0500

    Create a TAB Funtion

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,10 @@
 import { Stack } from "expo-router";
-import { StatusBar, } from "react-native";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
     <>
-<Stack screenOptions={{headerShown : true}}>
-      <Stack.Screen name="index" />
-    </Stack>
+      <Stack screenOptions={{ headerShown: false }} />
     </>
   );
 }
