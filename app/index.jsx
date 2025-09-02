68be94a0418f20e7fd7367f01009778a8b9b54e7
mabubakarsiddiq11 <mabubakarsiddiq11@gmail.com>
2025-09-02 12:13:58 +0500

    FlatList & ScrollView

--- app/index.jsx
+++ app/index.jsx
@@ -1,29 +1,60 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { ScrollView } from "react-native";
+import { FlatList } from "react-native";
 
 const Home = () => {
   return (
-    <ScrollView
-     showsHorizontalScrollIndicator={false}
-     horizontal
-    //  contentContainerStyle={} // styling ke liye
-     >
-      {[1, 2, 3, 1, 2, 2, 2, 2, 3, 3, 21, 7].map((item) => {
-        return (
+    // Scroll View Concept
+    // <ScrollView
+    //  showsHorizontalScrollIndicator={false}
+    //  horizontal
+    // //  contentContainerStyle={} // styling ke liye
+    //  >
+    //   {[1, 2, 3, 1, 2, 2, 2, 2, 3, 3, 21, 7].map((item) => {
+    //     return (
+    //       <View
+    //         style={{
+    //           width: 80,
+    //           height: 80,
+    //           backgroundColor: "red",
+    //           borderRadius: 50,
+    //           marginRight : 10,
+    //           marginTop : 10
+    //         }}
+    //       ></View>
+    //     );
+    //   })}
+    // </ScrollView>
+
+    <View>
+      <FlatList
+      // ListHeaderComponent={}
+        // ListFooterComponent={}
+        // numColumns={}
+        data={[1, 2, 3, 1, 2, 2, 2, 2, 3, 3, 21, 7]}
+        horizontal
+        renderItem={({item}) => {
+          return (
+            
           <View
             style={{
               width: 80,
               height: 80,
               backgroundColor: "red",
               borderRadius: 50,
-              marginRight : 10,
-              marginTop : 10
+              marginRight: 10,
+              marginTop: 10,
+              justifyContent : "center",
+              alignItems : "center"
             }}
-          ></View>
-        );
-      })}
-    </ScrollView>
+          >
+            <Text>{item}</Text>
+          </View>
+          )
+        }}
+      />
+    </View>
   );
 };
 
