f75a17b318d8564e656fa4c6aa946beecf4a3a7b
mabubakarsiddiq11 <mabubakarsiddiq11@gmail.com>
2025-09-05 22:43:38 +0500

    Create a custom modal

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,38 @@
 import { View, Text, Modal } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import MyBtn from "../Components/MyBtn";
+import { StyleSheet } from "react-native";
 
 const Home = () => {
+  const [visible, setVisible] = useState(false);
   return (
-   <>
-    <Modal>
-      <View style={{backgroundColor : "blue", flex: 1 }}>
-        <View style={{backgroundColor : "blue"}}></View>
+    <>
+      <View style={styles.container}>
+        <MyBtn title={"Open Modal"} onPress={() => setVisible(true)} />
+        <Modal visible={visible} animationType="slide" transparent>
+          <View style={styles.modalContainer}>
+       <View style={{backgroundColor : "blue", height : 300 , width : "full"}}>
+         <MyBtn title={"Close Modal"} onPress={() => setVisible(false)} />
+       </View>
+          </View>
+        </Modal>
       </View>
-      <Text>Home</Text>
-    </Modal>
-   </>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContainer: {
+    flex: 1, 
+    backgroundColor: "transparent",
+    justifyContent: "flex-end",
+  },
+
+});
 
 export default Home;
